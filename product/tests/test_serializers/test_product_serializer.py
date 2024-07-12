63a8d805a173a464e8553b65fe636a2555707bb0
Jean Casimiro <casimirojean2001@gmail.com>
2024-07-12 01:23:15 -0300

    ViewSets and tests creation

--- product/tests/test_serializers/test_product_serializer.py
+++ product/tests/test_serializers/test_product_serializer.py
@@ -14,11 +14,7 @@ class TestProductSerializer(TestCase):
 
     def test_product_serializer(self):
         serializer_data = self.product_serializer.data
-        self.assertEquals(serializer_data["price"], 100)
-        self.assertEquals(serializer_data["title"], "mouse")
-        self.assertEquals(
+        self.assertEqual(serializer_data["price"], 100)
+        self.assertEqual(serializer_data["title"], "mouse")
+        self.assertEqual(
             serializer_data["category"][0]["title"], "technology")
-
-    def test_product_serializer_fields(self):
-        serializer_data = self.product_serializer.data
-        self.assertEqual(set(serializer_data.keys()), set(['title', 'description', 'price', 'active', 'category']))    
